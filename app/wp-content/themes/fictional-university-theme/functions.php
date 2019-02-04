4ca75bee68897ef17acc489db4fd9984e51e9ceb
Oscar Miras <oscar.miras@gmail.com>
2019-02-04 22:57:43 +0100

    Add some custom posts and custom fields. ACF plugin installed

--- app/wp-content/themes/fictional-university-theme/functions.php
+++ app/wp-content/themes/fictional-university-theme/functions.php
@@ -17,3 +17,33 @@ function university_features() {
 }
 
 add_action('after_setup_theme', 'university_features');
+
+function university_adjust_queries($query) {
+
+    $today = date('Ymd');
+
+
+    if (!is_admin() and is_post_type_archive('event') and  $query->is_main_query()) {
+        $query->set('meta_key', 'event_date');
+        $query->set('orderby', 'meta_value_num');
+        $query->set('order', ASC);
+        $query->set('meta_key', 'event_date');
+        $query->set('meta_query', array(
+            array(
+              'key' => 'event_date',
+              'compare' => '>=',
+              'value' => $today,
+              'type' => 'numeric'
+            )
+          ));
+    }
+
+    if (!is_admin() and is_post_type_archive('program') and  $query->is_main_query()) {
+        $query->set('orderby', 'title');
+        $query->set('order', ASC);
+        $query->set('posts_per_page', -1);
+ 
+    }
+}
+
+add_action('pre_get_posts', 'university_adjust_queries');
